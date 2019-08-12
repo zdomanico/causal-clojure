75244b088d6b4e1c5a02383137fd789a639fc684
Zach Domanico <zdomanico@gmail.com>
2019-08-11 20:22:22 -0400

    Added create-graph tests.

--- test/causal_clojure/core_test.clj
+++ test/causal_clojure/core_test.clj
@@ -1,7 +1,50 @@
 (ns causal-clojure.core-test
   (:require [clojure.test :refer :all]
-            [causal-clojure.core :refer :all]))
+            [causal-clojure.core :refer :all]
+            [causal-clojure.graph :as graph]
+            [causal-clojure.decisiontree :as dt]
+  )
+)
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest create-graph
+  (testing "Create Empty Graph"
+    (is 
+      (= 
+        {}
+        (graph/create-graph [])
+      )
+    )
+  )
+  (testing "Create Connected Graph"
+    (is 
+      (= 
+        {:A #{:B :C}, :B #{:A :C}, :C #{:A :B}}
+        (graph/create-graph [[:A :B :C] [:B :A :C] [:C :A :B]])
+      )
+    )
+  )
+  (testing "Create Graph with one source and 3 leafs 1/2"
+    (is 
+      (= 
+        {:A #{:B :C :D}, :B #{}, :C #{} :D #{}}
+        (graph/create-graph [[:A :B :C :D]])
+      )
+    )
+  )
+  (testing "Create Graph with one source and 3 leafs 2/2"
+    (is 
+      (= 
+        {:A #{:B :C :D}, :B #{}, :C #{} :D #{}}
+        (graph/create-graph [[:A :B :C :D] [:B]])
+      )
+    )
+  )
+  (testing "Create Graph with three singleton nodes"
+    (is 
+      (= 
+        {:A #{}, :B #{}, :C #{}}
+        (graph/create-graph [[:A] [:B] [:C]])
+      )
+    )
+  )
+)
